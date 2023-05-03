855c915298306c3215e8988f5628334583905fb6
Sagar <sagar785mishra@gmail.com>
2023-05-03 10:55:46 +0530

    Second Commit

---
+++ quick_sort.js
@@ -0,0 +1,21 @@
+const quickSort = (arr)=>{
+    if(arr.length === 0){
+        return [];
+    }
+    let pivot = arr[0];
+    let leftSide = [];
+    let rightSide = [];
+    for(let i=1;i<arr.length;i++){
+        if(pivot>arr[i]){
+            leftSide.push(arr[i]);
+        }
+        else {
+            rightSide.push(arr[i]);
+        }
+    }
+    return quickSort(leftSide).concat(pivot, quickSort(rightSide));
+}
+
+let arr = [2,5,7,3,6,4];
+console.log(arr);
+console.log(quickSort(arr));
